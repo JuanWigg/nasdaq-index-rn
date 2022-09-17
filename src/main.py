cc0c052f9ba49870c5295999e4b2ce69eb7a3983
jw-tera <juan.wiggenhauser@teracloud.io>
2022-09-16 23:48:40 -0300

    Changing END DATE for dataset query

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 import nasdaq as nq 
 
 # From https://data.nasdaq.com/
-dataset = nq.getDataset('LBMA/SILVER', start_date='2015-12-31', end_date='2016-12-31')
+dataset = nq.getDataset('LBMA/SILVER', start_date='2015-12-31', end_date='2020-12-31')
 print(dataset)
