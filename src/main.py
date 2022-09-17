10e2c2c4fe019092c585ef73a26b1e273f244609
jw-tera <juan.wiggenhauser@teracloud.io>
2022-09-17 13:14:38 -0300

    Adding weekDay calculation

--- src/main.py
+++ src/main.py
@@ -1,5 +1,22 @@
 import nasdaq as nq 
+import util
 
 # From https://data.nasdaq.com/
-dataset = nq.getDataset('LBMA/SILVER', start_date='2015-12-31', end_date='2020-12-31')
+dataset = nq.getDataset('LBMA/SILVER', start_date='2022-01-31', end_date='2022-06-06')
+
+print("============== Dataset ================\n")
 print(dataset)
+
+print("=========== Adding Weekday ============\n")
+dataset['weekDay'] = dataset.apply(lambda row: util.getWeekDayValue(row.name), axis=1)
+print(dataset)
+
+print("========== Splitting dataset ==========\n")
+first_70days, last_days = util.splitDataframe(dataset)
+
+print("First portion: \n")
+print(first_70days)
+
+print("Last portion: \n")
+print(last_days)
+
--- src/util.py
+++ src/util.py
@@ -1,6 +1,13 @@
 from datetime import datetime
+import numpy as np 
+import pandas as pd
 
-def getDayOfWeek(date):
-    # Must be a string in YYYY-MM-DD
-    date_object = datetime.strptime(date, "%Y\%m\%d")
-    return date_object.isoweekday()
+def getWeekDayValue(index):
+    date_object = pd.to_datetime(index)
+    return np.sin(date_object.isoweekday() * (2* np.pi / 7))
+
+
+def splitDataframe(dataframe):
+    first_70days = dataframe.iloc[:70, :]
+    last_days = dataframe.iloc[70:, :]
+    return first_70days, last_days
