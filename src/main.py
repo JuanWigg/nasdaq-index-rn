822878a8ffd88b1e7d97470b5966b0a7a74f5b5a
jw-tera <juan.wiggenhauser@teracloud.io>
2022-09-18 19:21:59 -0300

    Adding dataset as CSV and filtering it

--- src/main.py
+++ src/main.py
@@ -1,20 +1,42 @@
-import nasdaq as nq 
 import pandas as pd
+import numpy as np
 import util
+import matplotlib.pyplot as plt
 
-# From https://data.nasdaq.com/
-dataset = nq.getDataset('LBMA/SILVER', start_date='2022-01-31', end_date='2022-06-06')
+dataset = pd.read_csv('data/nasdaq_index.csv', parse_dates=[0])
+dataset = dataset.rename(columns={"Close/Last": "Close"})
+filtered_dataset = dataset.query("Date >= '2015-01-28' and Date <= '2015-06-18'" )
+sorted_dataset = filtered_dataset[::-1]
+sorted_dataset = sorted_dataset.reset_index()
+sorted_dataset = sorted_dataset.drop(columns=['Volume'])
+sorted_dataset['Open'] = np.where(sorted_dataset['Open']==0, sorted_dataset['Close'], sorted_dataset['Open'])
+sorted_dataset['High'] = np.where(sorted_dataset['High']==0, sorted_dataset['Close'], sorted_dataset['High'])
+sorted_dataset['Low'] = np.where(sorted_dataset['Low']==0, sorted_dataset['Close'], sorted_dataset['Low'])
+test_dataset = sorted_dataset.query("Open == 0")
+
+
 
 print("============== Dataset ================\n")
 print(dataset)
 
-print("=========== Adding Weekday ============\n")
-dataset['weekDayValue'] = dataset.apply(lambda row: util.getWeekDayValue(row.name), axis=1)
-dataset['weekDay'] = dataset.apply(lambda row: pd.to_datetime(row.name).day_name(), axis=1)
-print(dataset)
+print("============== Test Dataset ================\n")
+print(test_dataset)
 
-print("========== Splitting dataset ==========\n")
-first_70days, last_days = util.splitDataframe(dataset)
+
+plt.plot(sorted_dataset['Close'], marker='s')
+plt.xlabel("Date")
+plt.ylabel("Algo")
+plt.show()
+
+
+print("=========== Adding Weekday ============\n")
+sorted_dataset['weekDayValueSin'] = sorted_dataset.apply(lambda row: util.getWeekDayValueSin(row['Date']), axis=1)
+sorted_dataset['weekDayValueCos'] = sorted_dataset.apply(lambda row: util.getWeekDayValueCos(row['Date']), axis=1)
+sorted_dataset['weekDay'] = sorted_dataset.apply(lambda row: pd.to_datetime(row['Date']).day_name(), axis=1)
+print(sorted_dataset)
+
+print("========== Splitting Dataset ==========\n")
+first_70days, last_days = util.splitDataframe(sorted_dataset)
 
 print("First portion: \n")
 print(first_70days)
--- src/util.py
+++ src/util.py
@@ -2,10 +2,13 @@ from datetime import datetime
 import numpy as np 
 import pandas as pd
 
-def getWeekDayValue(index):
+def getWeekDayValueSin(index):
     date_object = pd.to_datetime(index)
     return np.sin(date_object.isoweekday() * (2* np.pi / 7))
 
+def getWeekDayValueCos(index):
+    date_object = pd.to_datetime(index)
+    return np.cos(date_object.isoweekday() * (2* np.pi / 7))
 
 def splitDataframe(dataframe):
     first_70days = dataframe.iloc[:70, :]
