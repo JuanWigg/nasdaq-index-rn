7b98542be21f1de3c0f442adc0a0702a27286b85
jw-tera <juan.wiggenhauser@teracloud.io>
2022-10-04 16:49:40 -0300

    Fixing plot and doing paper training

--- src/tests.py
+++ src/tests.py
@@ -10,8 +10,8 @@ from datetime import datetime
 
 myTrainset = Trainset()
 
-myTrainset.fecha_inicio_dataset= "2015-01-01"
-myTrainset.fecha_fin_dataset= "2015-12-31"
+myTrainset.fecha_inicio_dataset= "2013-04-02"
+myTrainset.fecha_fin_dataset= "2015-06-18"
 myTrainset.dias_hacia_atras = 4
 myTrainset.porcentaje_entrenamiento = 0.7
 myTrainset.porcentaje_validacion = 0.15
@@ -24,20 +24,22 @@ validation_inputs, validation_outputs = dataset_manager.generateTrainingIOs(vali
 test_inputs, test_outputs = dataset_manager.generateTrainingIOs(test_days, myTrainset.dias_hacia_atras)
 
 print("Cargando modelo...")
-model = tf.keras.models.load_model("models/model.h5")
+model = tf.keras.models.load_model("models/model-paper.h5")
 
 predictions = [item for sublist in model.predict(test_inputs) for item in sublist]
 results_df = pd.DataFrame(columns=['Date', 'Prediction', 'RealValue'])
 results_df["Date"] = test_days["Date"].iloc[myTrainset.dias_hacia_atras:].apply(lambda row: (pd.to_datetime(row)).strftime("%Y-%m-%d"))
 results_df["RealValue"] = test_days["Close"].iloc[myTrainset.dias_hacia_atras:]
 results_df["Prediction"] = predictions
+results_df.reset_index()
+
 
 plt.plot(results_df["Prediction"], marker='o', color='green')
 plt.xlabel("Fecha")
-plt.xticks(ticks=range(0,len(results_df)), labels=results_df["Date"], rotation=90)
 plt.ylabel("Valor de cierre")
 plt.plot(results_df["RealValue"], marker="s", color='red')
-
+plt.xticks(ticks=range(results_df.index[0], results_df.index[-1]+1), labels=results_df["Date"], rotation=90)
+plt.grid()
 print(f'R2: {r2_score(results_df["RealValue"], results_df["Prediction"])}')
 
 plt.show()
